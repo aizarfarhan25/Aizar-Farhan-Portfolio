1fd11765c47a152cb28e0a3903b5825f3f1007eb
Aizar Farhan <andiaizarfarhan@gmail.com>
2024-10-14 20:43:45 +0700

    update project

---
+++ src/components/Project/Project Card/ProjectCard.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './ProjectCard.scss'
+import { link } from 'framer-motion/client'
+
+const Project = () => {
+  const projects = [
+    {
+      title: 'LaporAja',
+      company: 'Project Binus',
+      description:
+        'is a mobile application that enables users to report criminal activities quickly and safely.',
+      time: '2023',
+      link: 'https://www.figma.com/proto/FIUgohVL7E5eeFp9V2X5cV/ABAD-PROJECT?page-id=0%3A1&node-id=17-58&node-type=canvas&viewport=796%2C350%2C0.35&t=JVJFuKUqHcyQhgEX-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=5%3A2&show-proto-sidebar=1',
+    },
+    {
+      title: 'PlantPedia',
+      company: 'Project Binus',
+      description:
+        ' is a mobile application that provides users with information about various types of plants.',
+      time: '2023',
+      link: 'https://www.figma.com/proto/ztoeI7e5lYQfUyvJtM2x0e/Final-Project-UI-UX-(Copy)?page-id=0%3A1&node-id=2-285&node-type=canvas&viewport=548%2C435%2C0.37&t=tlVegI9UMbAs9Toy-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=2%3A2&show-proto-sidebar=1'
+    },
+    {
+      title: 'Coming Soon..',
+      company: '...',
+      description: 'This project is not finished yet.',
+      time: 'Coming Soon',
+    },
+    // Add more project objects as needed
+  ]
+
+  return (
+    <div className="project-container project-margin">
+      {projects.map((project, index) => (
+        <div key={index} className="project-card">
+          <div className="project-header">
+            <span>{project.time}</span>
+          </div>
+          <div className="project-content">
+            <h2>{project.title}</h2>
+            <p>{project.company}</p>
+            <p>{project.description}</p>
+           <a href={project.link} target='_blank'><button>see more</button></a>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Project
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,40 +1,45 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import ProjectCard from './Project Card/ProjectCard'
 
 const Project = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    useEffect(() => {
-      const timeoutId = setTimeout(() => {
-        setLetterClass('text-animate-hover')
-      }, 3000)
-    }, [])
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
-    return (
-        <>
-            <div className='container project-page'>
-                <div className='text-zone'>
-                    <h1>
-                        <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={['M', 'y', ' ', 'W', 'o', 'r', 'k', 's']}
-                        idx={15}
-                        />
-                    </h1>
-                    <p>
-                        <b>Welcome to My Project Gallary</b>
-                        <br/>
-                        Explore the collection of my professional and personal works, showcasing my passion for
-                        development, creativity, and problem-solving. Each project reflects my dedication to 
-                        innovation and growth in the field of technology and design.
-                    </p>
-                </div>
-            </div>
-            <Loader type="pacman" />        
-        </>
-    )
+  return (
+    <>
+      <div className="container project-page">
+        <div className="text-zones">
+          <div className="text-width">
+            <h1>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={['M', 'y', ' ', 'W', 'o', 'r', 'k', 's']}
+                idx={15}
+              />
+            </h1>
+            <p>
+              <b>Welcome to My Project Gallary</b>
+              <br />
+              Explore the collection of my professional and personal works,
+              showcasing my passion for development, creativity, and
+              problem-solving. Each project reflects my dedication to innovation
+              and growth in the field of technology and design.
+            </p>
+          </div>
+          <ProjectCard />
+        </div>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
 }
-
 export default Project
